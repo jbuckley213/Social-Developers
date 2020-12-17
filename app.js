5342b9f4a2952dbd36772934dfb766ca994361e8
Joseph Buckley <jbuckley213@gmail.com>
2020-12-17 09:35:29 +0100

    Add middleware for serving react app

--- app.js
+++ app.js
@@ -71,6 +71,11 @@ app.use("/api/posts", postRouter)
 app.use('/api/conversations', conversationRouter)
 
 
+// ROUTE FOR SERVING REACT APP (index.html)
+app.use((req, res, next) => {
+  // If no previous routes match the request, send back the React app.
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 
 // ERROR HANDLING
